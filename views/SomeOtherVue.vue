c2e0da6b447ce9ed6dee165e68be1706c99d201f
Wayne Roddy <dubrod@icloud.com>
2021-09-27 16:35:48 -0500

    Create SomeOtherVue.vue

---
+++ views/SomeOtherVue.vue
@@ -0,0 +1,16 @@
+<template>
+  ...
+  <button v-on:click="showNote(value)">Button with a purpose</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    showNote(value){
+        this.$root.notifications = {
+          msg: 'Are you sure you want to delete ' + value
+        }
+    }
+  }
+}
+</script>
