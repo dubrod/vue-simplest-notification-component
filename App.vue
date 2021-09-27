faddaaea9b74d4106b2d5823c9c7ce99831417da
Wayne Roddy <dubrod@icloud.com>
2021-09-27 16:31:03 -0500

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  ...
+  <NotificationsMessage :msg="notifications.msg" />
+</template>
+
+<script>
+import NotificationsMessage from '@/components/NotificationsMessage.vue'
+
+export default {
+  data() {
+    return {
+      notifications: {
+        msg: ''
+      }
+    }
+  },
+  components: {
+    NotificationsMessage
+  }
+}
+</script>
